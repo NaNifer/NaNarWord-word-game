fd1f8b7d291abf626a46adc465cfcfe5cc4b5b49
Unicorn Barf <nolan.spence7@gmail.com>
2022-06-04 13:27:17 -0700

    Added dynamically created gameplay section and event listener for input keyboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
+// Global Variables
+// Nolan
+var word;
 
-// start button event listener
+
+
+// // start button event listener
 var startBtn = document.getElementById("start");
 element.addEventListener("click", startGame);
 
@@ -15,20 +20,28 @@ function startGame() {
 // Event Listener for Level Selection
 $("#level-div").on("click", "button", function(event) {
     let btnEl = event.target;
-    let level = btnEl.data.level;
+    let level = $(btnEl).data("level");
     console.log(`The level is ${level}`);
     // call word search function with level of word
+    randomWordFetch(level);
     // figure what to hide and show
 })
 
 // Nolan
 // Words API random fetch
 function randomWordFetch(level) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+            'X-RapidAPI-Key': '4d97b98fbamshd06a775c8ed3df9p1429d2jsnbf625cf74463'
+        }
+    };
     fetch(`https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=4&lettersMax=7&frequencyMax=${level}/definitions`, options)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            merriamFetch(data.word);
+            word = data.word;
+            gameScreen()
         })
         .catch(err => console.error(err));
 }
@@ -43,3 +56,50 @@ function merriamFetch(word) {
     .catch(err => console.error(err));
 }
 
+// Nolan
+// Function to print gameplay Screen
+function gameScreen() {
+    // Create a div element to hold the guessing letters
+    let guessDiv = $('<div id="guess-div"></div>');
+    // Loop to create Empty word elements to guess
+    for (i=0; i<word.length; i++) {
+        let guess = $("<h2>")
+            .addClass("guess-el")
+            .data("letter", word[i])
+            .css({"border-bottom": "8px solid black", "display": "inline-block", "width": "75px", "margin": "0 10px"});
+        guessDiv.append(guess);
+    }
+    // Create array of uppercased letters
+    let keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    keys.split('');
+    // create a key div with id=key-div
+    let keyDiv = $('<div id="key-div"></div>');
+    // Loop to create key div elements for keyboard
+    for (i=0; i<keys.length; i++) {
+        let keyEl = $("<button>")
+            .addClass("key-el")
+            .data("letter", keys[i])
+            .text(keys[i]);
+        keyDiv.append(keyEl);
+    }
+    // Append to the game container div
+    $("#game-div").append(guessDiv, keyDiv)
+}
+
+// Nolan
+// Event handlers for the generated keyboard
+$("#game-div").on("click", "button", function(event) {
+    let btnEl = event.target;
+    // Store buttons data-letter as guess
+    let guess = $(btnEl).data("letter");
+    // Check if letter clicked is in the word string
+    if (word.includes(guess)) {
+        // loop to fill in the correct letter spaces
+        for (i=0; i<word.length; i++) {
+            let slotEl = $("#guess-div").child(i).data("letter");
+            if (guess === slotEl) {
+                slotEl.text(guess);
+            }
+        }
+    }
+});
