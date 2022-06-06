9f978572b15acc1ea53e37b3a378e00e0ea18d88
Nifer Kilakila <nifercritter@gmail.com>
2022-06-06 15:57:00 -0700

    refactored giphy call, storage function, end game

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,7 +164,7 @@ function getGiphy(query, win) {
     const API_KEY = "dzRUlVy8AmnIrMfFmPikr7L2vL8qqV97";
     let requestUrl = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=1`;
 
-    fetch(requestUrl)
+    fetch(requestUrl)re
         .then(function (response) {
             return response.json();
         })
