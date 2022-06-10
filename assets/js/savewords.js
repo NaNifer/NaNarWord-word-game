4241bb51975ed928f17e32060e090e81380e2157
Nifer Kilakila <nifercritter@gmail.com>
2022-06-09 22:49:17 -0700

    Refactored savewords.js, functioning dynamic word definition btns (once it is checked with Angie's html

--- assets/js/savewords.js
+++ assets/js/savewords.js
@@ -2,6 +2,8 @@
 let wordList = JSON.parse(localStorage.getItem("wordList")) || [];
 let buttonData;
 
+// Nifer
+// Stores word and frequency into local storage, and sorts it with highest frequency first
 function storeWord(word, frequency) {
     let userWordInfo = {
         wordSaved: word,
@@ -13,52 +15,49 @@ function storeWord(word, frequency) {
     renderWordBoard(wordList);
 }
 
-
-// Creagte onclick, render the wordBoard
+// Nifer
+// Create onclick, render the wordBoard
 function renderWordBoard(wordList) {
-
     let wordBoardEl = document.getElementById("top-10");
     let wordListDiv = document.createElement("div");
     wordListDiv.classList.add("wordButtonContainer");
-
+    wordListDiv.innerHTML = "";
     for (let i = 0; i <= 10 && i < wordList.length; i++) {
         buttonData = `
-        <button class="btn" id="wordbutton" type="button">
-        <span id="rarity-rating">Rarity Rating: ${Math.ceil(wordList[i].level / 10) * 10}</span>
+        <a href="#modal-word-bank" class="modal-trigger">
+        <button id="wordbutton" type="button" data-target="modal-word-bank" class="btn modal-trigger" data-word=${wordList[i].wordSaved}>
          ${wordList[i].wordSaved}  
         </button>
+        </a>
         `
         wordListDiv.innerHTML += buttonData;
         wordBoardEl.appendChild(wordListDiv);
+        wordListDiv.addEventListener("click", function (event) {
+            let buttonEl = event.target
+            retrieveDefinition(buttonEl, wordList);
+        });
     }
 }
 
-
-// Click listener for Word Borad buttons
-$("#wordbutton").on("click", function (event) {
-    console.log(event)
-})
-
-
-
+// Nifer
 // Calls on definition and displays modal
-function retrieveDefinition() {
-    let recallWord = getElementById("#wordbutton").innerHTML;
+function retrieveDefinition(buttonEl, wordList) {
+    let appendDefEl = document.getElementById("append-definition");
+    console.log(appendDefEl);
+    // Empties div here
+    if (appendDefEl.hasChildNodes()) {
+        appendDefEl.innerHTML = "";
+    }
+    let recallWord = buttonEl.dataset.word;
+    console.log(recallWord);
     let defArray = grabWordDef(recallWord);
-    let DefinitionModal = `<div id="modal2" class="modal">
-    <div class="modal-content">
-        <h4 id="chosen-button-word">${defArray[0]}</h4>
-        <p id="chosen-button-speech">${defArray[1]}</p>
-        <p id="chosen-button-definition">${defArray[2]}</p>
-    </div>
-    <div class="modal-footer">
-        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Thanks!</a>
-    </div>
-</div>
-`
+    appendDefEl.append(defArray[0], defArray[1], defArray[2], "Rarity Rating: " + Math.ceil(wordList[i].level / 10) * 10);
 }
 
-$(document).ready(function () {
-    $('#modal2').modal();
+// Angie & Ivy
+// Loads the modal on page load and connects the elems to the modal instance.
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('#modal-word-bank');
+    let instances = M.Modal.init(elems, {});
 });
 
